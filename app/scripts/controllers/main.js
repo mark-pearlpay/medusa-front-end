d62ed96d2e9c38fd74a3e53ad5d7f4798d106803
Mark Kenneth Ranosa <mark@pearlpay.io>
2020-02-20 18:00:51 +0800

    Added wallet form and wallet list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,28 @@
  * Controller of the medusaFrontEndApp
  */
 angular.module('medusaFrontEndApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', [
+  	'$scope', 'WalletService', 
+  	function ($scope, WalletService) {
+	    $scope.wallets = [];
+
+	    // load list of wallets
+	    WalletService.list().then((data)=>{
+	    	$scope.wallets = data;
+	    });
+
+	    $scope.saveWallet = () => {
+	    	const wallet = {
+	    		firstName: $scope.firstName,
+				lastName: $scope.lastName,
+				email: $scope.email,
+				birthday: $scope.birthday,
+				balance: $scope.balance
+	    	};
+
+	    	WalletService.save(wallet).then((data)=>{
+		    	$scope.wallets = data;
+		    });
+	    };
+  	}
+]);
